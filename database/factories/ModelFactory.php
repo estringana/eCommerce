2e92ea1acd3a0a59338fff32d50ba87fb85f05cb
Alejandro Estringana Ruiz <estringana@gmail.com>
2017-07-22 19:29:17 +1000

    001 Add GET product endpoint to the API

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,6 +12,9 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
@@ -20,5 +23,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->paragraph(2),
     ];
 });
---
+++ tests/Feature/ProductsCanBeRequestedTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Product;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Teapot\StatusCode\Http;
+use Tests\TestCase;
+
+class ProductsCanBeRequestedTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->actingAs(factory(User::class)->create(), 'api');
+    }
+
+    /** @test */
+    public function itShouldReturn200WhenVisitingAnExistingProduct()
+    {
+        $this->disableExceptionHandling();
+
+        $product = factory(Product::class)->create();
+        $this->get('/api/products/' . $product->id)
+            ->assertStatus(Http::OK);
+    }
+
+    /** @test */
+    public function itShouldReturnNotFoundIfProductNotInDB()
+    {
+        $notExistingProductId = 12345;
+        $this->get('/api/products/' . $notExistingProductId)
+            ->assertStatus(Http::NOT_FOUND);
+    }
+
+    /** @test */
+    public function itShouldReturnAProductWithNameAndDescription()
+    {
+        $product = factory(Product::class)->create();
+        $response = $this->json('GET', '/api/products/' . $product->id);
+
+        $response->assertStatus(Http::OK)
+            ->assertJson([
+                Product::FIELD_NAME => $product->name,
+                Product::FIELD_DESCRIPTION => $product->description,
+            ]);
+    }
+}
